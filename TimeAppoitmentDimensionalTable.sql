a876bbdc43b3fa93dc954596559596955adcb445
Gabriela Luciu <81560429+GabrielaF25@users.noreply.github.com>
2026-04-01 20:56:42 +0300

    Delete TimeAppoitmentDimensionalTable.sql

--- TimeAppoitmentDimensionalTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW D_TIME_APPOINTMENT AS 
-SELECT DISTINCT 
-    appointment_date, 
-    EXTRACT(YEAR FROM appointment_date) AS year_no, 
-    EXTRACT(MONTH FROM appointment_date) AS month_no, 
-    TO_CHAR(appointment_date, 'YYYY-MM') AS year_month 
-FROM appointments; 
